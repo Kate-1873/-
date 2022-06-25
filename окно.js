7bba7649cdc574931182779775b33a10437c6c71
Kate-1873 <zyzkatezyz@gmail.com>
2022-06-25 21:39:39 +0400

    First commmit

---
+++ окно.js
@@ -0,0 +1,13 @@
+$(document).ready(function(){
+  //popup при загрузки стриницы    
+  PopUpHide();
+});
+  //отобразить popup
+function PopUpShow(){
+  $("#popup1").show();
+}
+  //скрыть popup
+function PopUpHide(){
+  $("#popup1").hide();
+}
+
